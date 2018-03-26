83b207554249994c364e079d1375f3b012a0f7bb
Ryan Noronha <ryannoronha1@Ryans-MacBook-Pro.local>
2018-03-26 10:59:32 -0400

    Assignment 8 completed

--- src/components/Album.js
+++ src/components/Album.js
@@ -55,6 +55,14 @@ class Album extends Component {
       this.play(newSong);
     }
 
+    handleNextClick() {
+      const currentIndex = this.state.album.songs.findIndex(song => this.state.currentSong === song);
+      const newIndex = Math.min(currentIndex + 1, 4);
+      const newSong = this.state.album.songs[newIndex];
+      this.setSong(newSong);
+      this.play(newSong);
+    }
+
    render() {
      return (
        <section className="album">
@@ -93,6 +101,7 @@ class Album extends Component {
            currentSong={this.state.currentSong}
            handleSongClick={() => this.handleSongClick(this.state.currentSong)}
            handlePrevClick={() => this.handlePrevClick()}
+           handleNextClick={() => this.handleNextClick()}
          />
        </section>
      );
