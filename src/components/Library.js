90d1082a5938ce1aa5850d6619c8923aa8e9bf08
Ryan Noronha <ryannoronha1@Ryans-MacBook-Pro.local>
2018-04-08 10:30:39 -0400

    resolved syntax issue in Library component

--- src/components/Library.js
+++ src/components/Library.js
@@ -10,7 +10,7 @@ class Library extends Component {
 
    render() {
     return (
-      <section className='library'>
+      <section className='library'> 
       {
         this.state.albums.map( (album, index) =>
           <div className="Album" key={index}>
